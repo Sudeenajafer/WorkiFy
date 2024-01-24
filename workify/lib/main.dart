5f8fd304cce0684b283864f763bbb9a1419b2214
Sudeenajafer <120114232+Sudeenajafer@users.noreply.github.com>
2024-01-24 22:51:53 +0530

    old set

--- workify/lib/main.dart
+++ workify/lib/main.dart
@@ -1,62 +1,31 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart' show AnimatedSplashScreen, SplashTransition;
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:workify/screen/login.dart';
 
 //com.flutterflow.homeU
 
+import 'screen/login.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Splash Screen',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: Splash2(),
-      debugShowCheckedModeBanner: false,
+        debugShowCheckedModeBanner: false,
+        home: AnimatedSplashScreen(
+          splash: const Image(
+              image:NetworkImage('https://content.presentermedia.com/content/clipart/00002000/2823/looking_though_binoculars_pc_800_wht.jpg')),
+          duration: 5000,
+          splashTransition: SplashTransition.fadeTransition,
+          backgroundColor: Colors.white60,
+          nextScreen: Myloginpage(),
 
-    );
-  }
-}
-class Splash2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedSplashScreen(
-      duration: 5000,
-
-      //title: new Text('GeeksForGeeks'),
-
-
-      splash: const Image(
-
-          image:NetworkImage('https://content.presentermedia.com/content/clipart/00002000/2823/looking_though_binoculars_pc_800_wht.jpg')),
-      splashTransition: SplashTransition.fadeTransition,
-      backgroundColor: Colors.white60,
-      //loadingText: Text("Loading"),
-      photoSize: 100.0,
-      loaderColor: Colors.blue,
-
-      navigateAfterSeconds: new SecondScreen(),
-    ),
-
-  }
-}
-class SecondScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title:Text("GeeksForGeeks")),
-      body: Center(
-          child:Text("Home page",textScaleFactor: 2,)
-      ),
+        )
     );
   }
 }
--- workify/lib/screen/login.dart
+++ workify/lib/screen/login.dart
@@ -9,23 +9,125 @@ class Myloginpage extends StatefulWidget {
 
 class _MyloginpageState extends State<Myloginpage> {
   @override
-
-
-
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Container(height:100, width:100, color: Colors.white),
+        body: Container(
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                colors: [
+                  Colors.blueAccent,
+                  Colors.blue,
+                  Colors.blue,
+                ]
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(height: 80,),
+              const Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("Login",
+                      style: TextStyle(color: Colors.white, fontSize: 40),),
+                    SizedBox(height: 10,),
+                    Text("Welcome Back",
+                      style: TextStyle(color: Colors.white, fontSize: 18),),
 
-          ],
-        ),
-      ),
-    );
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20,),
+              Expanded(
+                child: Container(
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(60),
+                          topRight: Radius.circular(60))
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(30),
+                    child: Column(
+                      children: <Widget>[
+                        const SizedBox(height: 60,),
+                        Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: const [BoxShadow(
+                                  color: Color.fromRGBO(225, 95, 27, .3),
+                                  blurRadius: 20,
+                                  offset: Offset(0, 10)
+                              )
+                              ]
+                          ),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                padding: const EdgeInsets.all(10),
+                                decoration: const BoxDecoration(
+                                    border: Border(
+                                        bottom: BorderSide(color: Colors.grey))
+                                ),
+                                child: const TextField(
+                                  decoration: InputDecoration(
+                                      hintText: "Email or phone number",
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                padding: const EdgeInsets.all(10),
+                                decoration: const BoxDecoration(
+                                    border: Border(
+                                        bottom: BorderSide(color: Colors.grey))
+                                ),
+                                child: const TextField(
+                                  decoration: InputDecoration(
+                                      hintText: "password",
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 40,),
+                        const Text("Forget password?",
+                          style: TextStyle(color: Colors.grey),),
+                        const SizedBox(height: 40,),
+                        Container(
+                          height: 50,
+                          margin: const EdgeInsets.symmetric(horizontal: 50),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: Colors.blueAccent
+                          ),
+                          child: const Center(
+                            child: Text("Login", style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),),
+                          ),
+                        ),
+
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
+
   }
 }
+
+
 

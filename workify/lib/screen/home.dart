2119e625b9a7502786d0a631b50ddf5360658de2
Abhiram P Ashok <120114232+Sudeenajafer@users.noreply.github.com>
2024-02-05 12:39:21 +0530

    1

--- workify/lib/screen/home.dart
+++ workify/lib/screen/home.dart
@@ -1,3 +1,5 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter/material.dart';
 import 'search.dart';
 import 'splash.dart';
@@ -14,7 +16,9 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('WorkiFy'),
+        title: Text('WorkiFy',
+          style: TextStyle(
+          fontFamily: 'Pacifico',),
       ),
       drawer: Drawer(
         child: ListView(
@@ -42,23 +46,104 @@ class _HomePageState extends State<HomePage> {
               ),          ],
         ),
       ),
-        body:Container(
-          height: 240.0,
-          width: 240.0,
-          color: Colors.blue[50],
-          child: Align(
-            alignment: Alignment.center,
-            child: Text(
-              "Geeky Text",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        body:ListView(
+          children: [
+        CarouselSlider(
+        items: [
+        //1st Image of Slider
+          Container(
+            height: 240.0,
+            width: double.infinity,
+            color: Colors.blue[50],
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Driver",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ],
             ),
           ),
-        )
-     // floatingActionButton: FloatingActionButton(
-       // child: Icon(Icons.search),
-       // onPressed: () => showSearch(context: context, delegate: Search()),
-      //),
 
+
+
+          //2nd Image of Slider
+    Container(
+    height: 240.0,
+      width: double.infinity, // Set the width to fill the available space
+    color: Colors.blue[50],
+    child: Align(
+    alignment: Alignment.center,
+    child: Text(
+    "Data Entry or Virtual Assistant",
+    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    ),
+    ),
+    ),
+
+    //3rd Image of Slider
+    Container(
+    height: 240.0,
+      width: double.infinity, // Set the width to fill the available space
+    color: Colors.blue[50],
+    child: Align(
+    alignment: Alignment.center,
+    child: Text(
+    "Babysitter",
+    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    ),
+    ),
+    ),
+
+    //4th Image of Slider
+    Container(
+    height: 240.0,
+      width: double.infinity, // Set the width to fill the available space
+    color: Colors.blue[50],
+    child: Align(
+    alignment: Alignment.center,
+    child: Text(
+    "Cook",
+    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    ),
+    ),
+    ),
+
+    //5th Image of Slider
+    Container(
+    height: 240.0,
+      width: double.infinity, // Set the width to fill the available space
+    color: Colors.blue[50],
+    child: Align(
+    alignment: Alignment.center,
+    child: Text(
+    "Tutoring",
+    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    ),
+    ),
+    ),
+
+    ],
+
+    //Slider Container properties
+    options: CarouselOptions(
+    height: 180.0,
+    enlargeCenterPage: true,
+    autoPlay: true,
+    aspectRatio: 16 / 9,
+    autoPlayCurve: Curves.fastOutSlowIn,
+    enableInfiniteScroll: true,
+    autoPlayAnimationDuration: Duration(milliseconds: 800),
+    viewportFraction: 0.8,
+    ),
+    ),
+    ],
+    ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.search),
+        onPressed: () => showSearch(context: context, delegate: Search()),
+      ),
     );
   }
 }

c7127e2b3acf3556c76cd91f2607f3f20dc4b647
Sudeenajafer <120114232+Sudeenajafer@users.noreply.github.com>
2024-02-07 22:31:41 +0530

    5

--- workify/lib/database/demo.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:async';
-
-import 'packprice:path/path.dart';
-import 'packprice:sqflite/sqflite.dart';
-
-final Future<Database> database = openDatabase(
-    join(await getDatabasesPath(), 'details.db'),
-);
--- workify/lib/main.dart
+++ workify/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sqflite/sqflite.dart';
 import 'screen/splash.dart';
 // import 'package:get/get.dart';
 
